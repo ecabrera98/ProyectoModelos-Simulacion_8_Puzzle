942c742c0e1b0ea6b6df4e3da9270e7121902894
Valeria Hidalgo <60413558+hidalgobri@users.noreply.github.com>
2022-01-12 11:42:26 -0500

    refactor menu inicial

--- 8Puzzle/main.cpp
+++ 8Puzzle/main.cpp
@@ -20,8 +20,8 @@ using namespace std;
 
 int main()
 {
-    int rand_gen_initial_states = 20;
-    int op;
+    //int rand_gen_initial_states = 20;
+    int op = -1;
     srand ( time(NULL) );
 	const string input_Arr[20] = {"413705826", "813402765", "246583017", "038145672",
 	"301682475", "182043765", "301682475", "361285047", "261705834", "203714568", "501837264",
@@ -29,9 +29,11 @@ int main()
 	"804125376", "235107846"};
 
 
-        while(rand_gen_initial_states--) {
-             int RandIndex = rand() % 20;
-             puzzle s(input_Arr[RandIndex]); //generamos una matriz para los estados iniciales del Puzzle
+       do {
+
+             //generamos una matriz para los estados iniciales del Puzzle
+             int randIndex = rand() % 20;
+             puzzle s(input_Arr[randIndex]);
            //puzzle s("413705826"); //generamos una matriz de tipo Puzzle con el estado inicial "413705826"
 
 		     cout<<endl<<"-----------------------------------------------------"<<endl;
@@ -39,25 +41,37 @@ int main()
 		     cout<<endl<<"-----------------------------------------------------"<<endl;
 		     cout<<"Estado Inicial: "<<endl;
 		     s.desplegar();
+		     cout<<"Distancia manhattan actual: "<<s.distancia_manhattan()<<endl;
+		     cout<<"Distancia mismatch actual: "<<s.distancia_mismatch()<<endl;
+
+		     cout<<endl<<"~~~~~~~~~~~~~~~~~~M E N U~~~~~~~~~~~~~~~~~~~~~~~~~ "<<endl;
+		     cout<<endl<<"Seleccione una opcion: "<<endl;
+		     cout<<"0: Ejecutar puzzle actual f(n) Distmatch "<<endl;
+		     cout<<"1: Ejecutar puzzle actual f(n) Distancia Manhattan"<<endl;
+		     cout<<"2: Cambiar puzzle actual"<<endl;
+		     cout<<"3: Cerrar programa"<<endl;
 		     cout<<endl;
-		     cout<<endl<<"Seleccione el método que desea Usar: "<<endl;
-		     cout<<endl<<"1: Distmatch "<<endl;
-		     cout<<endl<<"2: Distancia Manhattan"<<endl;
-		     cout<<endl<<"Para finalizar el programa digite 0"<<endl;
-		     cout<<endl;
+		     cout<<endl<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ "<<endl;
 		     cin>>op;
 
-		     if(op==1){
-		        cout<<endl<<"Solución 8 PUZZLE usando Distmatch: "<<endl;
-		        comenzar_8PUZZLE(s, 0);
+
+		     switch (op){
+                case 0:
+                    cout<<endl<<"Solución 8 PUZZLE usando Distmatch: "<<endl;
+                    comenzar_8PUZZLE(s, 0);
+
+                    break;
+
+                case 1:
+                    cout<<endl<<"Solución 8 PUZZLE usando la Distancia Manhattan: "<<endl;
+                    comenzar_8PUZZLE(s, 1);
+                    break;
+                case 3:
+                    exit(0);
+                    break;
 		     }
-		     else if(op==2){
-                  cout<<endl<<"Solución 8 PUZZLE usando la Distancia Manhattan: "<<endl;
-		          comenzar_8PUZZLE(s, 1);
-		     }
-		     else
-		     exit(0);
-		}
+
+		} while(op != -1);
 
 		return 0;
 }
